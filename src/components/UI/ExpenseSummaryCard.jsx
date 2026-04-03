928c05ae0d78dcf3c2d10025930ccb89d4f9df5a
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-04-04 01:23:42 +0530

    code expence summary card component

---
+++ src/components/UI/ExpenseSummaryCard.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+
+const ExpenseSummaryCard = ({
+  lable,
+  amount,
+  icon: Icon,
+  colorClass,
+  iconColor,
+}) => {
+  return (
+    <div
+      className={`${colorClass ? colorClass : 'bg-amber-200'} p-5 rounded-xl flex items-end justify-between cursor-pointer`}
+    >
+      <div className="col gap-16 md:gap-12">
+        <p className="text-sm text-zinc-600 mb-1">{lable}</p>
+        <p className="text-2xl font-bold text-zinc-900">{amount}</p>
+
+        <div className={`p-2 rounded-full bg-white`}>
+          <Icon size={20} className={`${iconColor}`} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseSummaryCard;
