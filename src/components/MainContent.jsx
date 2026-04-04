530d9d7c074108b013a727f662c8cc64cdab5192
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-04-04 23:54:51 +0530

    add greeting section in main context

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,33 @@
+import { Bell, Search } from 'lucide-react';
+
 const MainContent = () => {
   return (
-    <div>
-      <h1>Main Content</h1>
+    <div className=" flex-1 space-y-8">
+      {/*Greeting*/}
+      <div className="p-6 flex items-center justify-between">
+        <div>
+          <h2 className="text-2xl tracking-tighter font-bold text-zinc-900 mb-1">
+            Welcom back, Jane
+          </h2>
+          <p className="text-sm text-zinc-600">
+            Track your spending, manage your budgets and control tour cask flow.
+          </p>
+        </div>
+
+        <div className="flex items-center space-x-4">
+          <button className="p-2 cursor-pointer rounded-full bg-zinc-100 hover:bg-zinc-200 transition-colors">
+            <Search size={18} className="text-zinc-600" />
+          </button>
+          <button className="p-2 cursor-pointer rounded-full bg-zinc-100 hover:bg-zinc-200 transition-colors">
+            <Bell size={18} className="text-zinc-600" />
+          </button>
+        </div>
+      </div>
+      {/*Expence Summary Cards*/}
+
+      {/*Recent T ransaction & Budgets*/}
+
+      {/*Shedule Payments*/}
     </div>
   );
 };
